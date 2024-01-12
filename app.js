94acec2bb6a4cf6ccc606f0a4f56c3c863af81fc
rahul.chowdhury <rahul.chowdhury@asurion.com>
2024-01-11 17:37:16 -0700

    add websocket capability

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const { App } = require('@slack/bolt');
 const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.APP_TOKEN,
+    socketMode: true
 });
+
+
+app.message('hey', async ({ command, say }) => {
+    try {
+        say('Hello World!');
+    } catch (error) {
+        console.log("err")
+        console.error(error);
+    }
+});
+
+
+
 
 /* Add functionality here */
 
 (async () => {
     // Start the app
-    await app.start(process.env.PORT || 3000);
+    await app.start(3000);
 
     console.log('⚡️ Bolt app is running!');
 })();
